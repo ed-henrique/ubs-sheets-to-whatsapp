247ff250d2a90b96fbfecf08008a4cf6f967aab2
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2024-09-05 13:16:13 -0400

    feat: adds whapi integration

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	_ "github.com/joho/godotenv/autoload"
@@ -12,12 +16,14 @@ import (
 )
 
 const (
-        spreadsheetReadRange = "NOTIFICAÇÕES (NÃO MEXER)!A2:B"
+	whapiBaseURL         = "https://gate.whapi.cloud/messages/text"
+	spreadsheetReadRange = "NOTIFICAÇÕES (NÃO MEXER)!A2:B"
 )
 
 var (
-	googleApiKey = os.Getenv("GOOGLE_API_KEY")
-        spreadsheetID = os.Getenv("GOOGLE_SPREADSHEET_ID")
+	whapiApiKey   = os.Getenv("WHAPI_API_KEY")
+	googleApiKey  = os.Getenv("GOOGLE_API_KEY")
+	spreadsheetID = os.Getenv("GOOGLE_SPREADSHEET_ID")
 )
 
 type pregnantWoman struct {
@@ -27,9 +33,60 @@ type pregnantWoman struct {
 	healthAgentNumber string
 }
 
+func sendMessage(to, message string) error {
+	body := struct {
+		To   string `json:"to"` 
+		Body string `json:"body"` 
+	}{
+		To: "55" + to,
+		Body: message,
+	}
+
+	bodyRaw, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(http.MethodPost, whapiBaseURL, bytes.NewBuffer(bodyRaw))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+whapiApiKey)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusCreated && res.StatusCode != http.StatusOK {
+		if res.ContentLength < 0 {
+			log.Fatalf("Unable to send message: %d", res.StatusCode)
+			return errors.New("Unable to send message")
+		}
+
+		resBody := make([]byte, res.ContentLength)
+
+		_, err = res.Body.Read(resBody)
+		if err != nil {
+			return err
+		}
+
+		defer res.Body.Close()
+
+		log.Fatalf("Unable to send message: %s", string(resBody))
+
+		return errors.New("Unable to send message")
+	}
+
+	return nil
+}
+
 func main() {
 	ctx := context.Background()
-        pregnantWomen := make([]pregnantWoman, 0)
+	pregnantWomen := make([]pregnantWoman, 0)
 
 	srv, err := sheets.NewService(ctx, option.WithAPIKey(googleApiKey))
 	if err != nil {
@@ -45,11 +102,11 @@ func main() {
 		fmt.Println("No data found.")
 	} else {
 		for _, row := range resp.Values {
-                        pw := pregnantWoman{
-                                name: row[0].(string),
-                                healthAgentNumber: row[1].(string),
-                        }
-                        pregnantWomen = append(pregnantWomen, pw)
+			pw := pregnantWoman{
+				name:              row[0].(string),
+				healthAgentNumber: row[1].(string),
+			}
+			pregnantWomen = append(pregnantWomen, pw)
 		}
 	}
 }
